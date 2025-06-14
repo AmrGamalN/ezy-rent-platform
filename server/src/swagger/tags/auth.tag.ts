ae5242f7b0e671362791236a5f9c3da36a90dd91
AmrGamalN <amr5179520@gmail.com>
2025-06-14 19:42:03 +0300

    setup server

---
+++ server/src/swagger/tags/auth.tag.ts
@@ -0,0 +1,10 @@
+/**
+ * @swagger
+ * tags:
+ *  - name: Auth
+ *    description: The Auth api management
+ *  - name: Login
+ *    description: The Login api management
+ *  - name: Register
+ *    description: The Register api management
+ */
---
+++ server/src/swagger/tags/item.tag.ts
@@ -0,0 +1,16 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: Category
+ *     description: Category Management API
+ *   - name: DraftItem
+ *     description: Draft Item Management API
+ *   - name: Item
+ *     description: Item Management API
+ *   - name: Payment
+ *     description: Payment Management API
+ *   - name: Review
+ *     description: Review Management API
+ *   - name: Wishlist
+ *     description: WishList Management API
+ */
