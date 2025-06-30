942977d474f5d19f0a4d2e07c801d7aa3a129609
Amr Gamal <amr180185@fci.bu.edu.eg>
2025-06-30 15:04:00 +0300

    feat(shared):create shared folder and push folder in npm organisation

---
+++ server/shared/src/index.ts
@@ -0,0 +1,7 @@
+export * from "zod";
+export * from "./types/response.type";
+export * from "./utils/safeParser.util";
+export * from "./utils/response.util";
+export * from "./utils/customError.util";
+// export * from "./config/winston.config";
+export * from "./middleware/handleError.middleware";
---
+++ server/shared/src/middleware/handleError.middleware.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from "express";
+import { ResponseOptions } from "../types/response.type";
+
+type funcExpress = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any> | void;
+type func = (...args: any[]) => Promise<ResponseOptions | void>;
+
+export class HandleError {
+  private static instance: HandleError;
+
+  static getInstance() {
+    if (!HandleError.instance) {
+      HandleError.instance = new HandleError();
+    }
+    return HandleError.instance;
+  }
+
+  handleError = (func: funcExpress) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        return await (func as funcExpress)(req, res, next);
+      } catch (err: any) {
+        next(err);
+      }
+    };
+  };
+
+  errorMiddleware = () => {
+    return (err: any, req: Request, res: Response, next: NextFunction) => {
+      res.status(err.status || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+        error: err.stack,
+      });
+    };
+  };
+
+  warpError = <T extends func>(func: T): T => {
+    return (async (
+      ...args: Parameters<T>
+    ): Promise<ResponseOptions<Awaited<ReturnType<T>>> | void> => {
+      try {
+        return await func(...args);
+      } catch (error: any) {
+        return {
+          success: false,
+          status: error.status || 500,
+          message:
+            error instanceof Error ? error.message : "Internal Server Error",
+        };
+      }
+    }) as T;
+  };
+}
