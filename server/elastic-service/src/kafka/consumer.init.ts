603e69726c448e84ff66cf8e1b4307cc1d337933
Amr Gamal <amr180185@fci.bu.edu.eg>
2025-06-30 23:07:41 +0300

    feat(elastic): implement car filter by using elastic search

--- server/elastic-service/src/kafka/consumer.init.ts
+++ server/elastic-service/src/kafka/consumer.init.ts
@@ -1,5 +1,4 @@
 import { CarConsumer } from "./consumer/car.consumer";
-
 export const kafkaStart = async () => {
   const carConsumer = CarConsumer.getInstance();
   await carConsumer.startCarConsumer();
--- server/elastic-service/src/swaggers/routes/car.route.ts
+++ server/elastic-service/src/swaggers/routes/car.route.ts
@@ -3,6 +3,7 @@
  * /elastic/car/{id}:
  *   get:
  *     summary: Get a car by ID from Elasticsearch
+ *     description: Get a car by ID from Elasticsearch
  *     tags: [ElasticSearch]
  *     parameters:
  *       - in: path
@@ -29,9 +30,50 @@
 
 /**
  * @swagger
+ * /elastic/car/seacrh:
+ *   get:
+ *     summary: Search for cars in Elasticsearch
+ *     description: Search for cars in Elasticsearch
+ *     tags: [ElasticSearch]
+ *     parameters:
+ *       - $ref: '#/components/parameters/Page'
+ *       - $ref: '#/components/parameters/Limit'
+ *       - $ref: '#/components/parameters/Name'
+ *       - $ref: '#/components/parameters/Brand'
+ *       - $ref: '#/components/parameters/CarModel'
+ *       - $ref: '#/components/parameters/Year'
+ *       - $ref: '#/components/parameters/Color'
+ *       - $ref: '#/components/parameters/AvailableFrom'
+ *       - $ref: '#/components/parameters/AvailableTo'
+ *       - $ref: '#/components/parameters/City'
+ *       - $ref: '#/components/parameters/MinPrice'
+ *       - $ref: '#/components/parameters/MaxPrice'
+ *     responses:
+ *       200:
+ *         description: Cars found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusText:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *       404:
+ *         description: Cars not found
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
  * /elastic/car/mapping:
  *   post:
  *     summary: Create an Elasticsearch index mapping
+ *     description: Create an Elasticsearch index mapping
  *     tags: [ElasticSearch]
  *     responses:
  *       200:
