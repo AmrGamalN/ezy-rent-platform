a21afd4a7eff08cd7d571f52eed3bde5e243fce0
Amr Gamal <amr180185@fci.bu.edu.eg>
2025-07-05 15:57:49 +0300

    fix(payment): fix missing/incorrect TypeScript types

--- server/elastic-service/src/types/car.type.ts
+++ server/elastic-service/src/types/car.type.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export type SearchCar = {
   name: string;
   isAvailable: boolean;
@@ -14,6 +15,8 @@ export type SearchCar = {
   category: string;
 };
 
+=======
+>>>>>>> 663f8f9 (fix(payment): fix missing/incorrect TypeScript types)
 export type CreateCar = {
   userId: string;
   prefix: string;
@@ -28,6 +31,7 @@ export type CreateCar = {
   carImages: {
     url: string;
     key: string;
+<<<<<<< HEAD
   }[];
   price: number;
   discount: number;
@@ -71,6 +75,9 @@ export type UpdateCar = {
   carImages: {
     url: string;
     key: string;
+=======
+    prefix?: string;
+>>>>>>> 663f8f9 (fix(payment): fix missing/incorrect TypeScript types)
   }[];
   price: number;
   discount: number;
@@ -94,9 +101,72 @@ export type UpdateCar = {
     depositAmount?: number;
     additionalNotes?: string;
   };
+<<<<<<< HEAD
   isExpired?: boolean;
   expired_At?: Date;
   createdAt?: string;
   updatedAt?: string;
   __v?: number;
+=======
+  isExpired: boolean;
+  expired_At: Date;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type UpdateCar = {
+  phone: string;
+  name: string;
+  description: string;
+  carModel: string;
+  brand: string;
+  year: number;
+  color: string;
+  category: string;
+  carImages: {
+    url: string;
+    key: string;
+    prefix?: string;
+  }[];
+  price: number;
+  discount: number;
+  availableFrom: Date;
+  availableTo: Date;
+  location: {
+    city: string;
+    address: string;
+    coordinates?: {
+      lat?: string;
+      lng?: string;
+    };
+  };
+  isAvailable?: boolean;
+  allowNegotiate?: boolean;
+  guarantees?: {
+    hasInsurance?: boolean;
+    insuranceDetails?: string;
+    licenseValid?: boolean;
+    requiresDeposit?: boolean;
+    depositAmount?: number;
+    additionalNotes?: string;
+  };
+  isExpired: boolean;
+  expired_At: Date;
+};
+
+export type SearchCar = {
+  name: string;
+  brand: string;
+  carModel: string;
+  category: string;
+  color: string;
+  city: string;
+  year: number;
+  minPrice: number;
+  maxPrice: number;
+  isAvailable: boolean;
+  allowNegotiate: boolean;
+  availableFrom: Date;
+  availableTo: Date;
+>>>>>>> 663f8f9 (fix(payment): fix missing/incorrect TypeScript types)
 };
